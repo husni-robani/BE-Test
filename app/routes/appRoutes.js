e3c87905d1c19d880d1db277fe3329fae9ccb783
husni-robani <husnir2005@gmail.com>
2024-09-12 01:33:07 +0700

    make new routes and setup route

---
+++ app/routes/appRoutes.js
@@ -0,0 +1,18 @@
+const Controller = require("../controllers/appController");
+
+module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  const router = require("express").Router();
+
+  router.get("/survey", Controller.refactoreMe1);
+  router.post("/survey", Controller.refactoreMe2);
+
+  app.use("/api/data", router);
+};
