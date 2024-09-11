303c9e27768af06f8ab7a4ddf4b9e97a6fee72f3
husni-robani <husnir2005@gmail.com>
2024-09-12 01:33:51 +0700

    implmenting route and controller

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// database
-const db = require("./app/models");
-
-db.sequelize.sync();
+// ========================================== database (comment by me)
+// const db = require("./app/models");
+// db.sequelize.sync();
+// ==========================================
 
 // never enable the code below in production
 // force: true will drop the table if it already exists
@@ -35,10 +35,10 @@ app.get("/", (req, res) => {
 });
 
 // routes
-// require("./app/routes/exaole.routes")(app);
+require("./app/routes/appRoutes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 7878;
+const PORT = 7878;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
