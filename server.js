92ed7cf9861928626fedfdec72084c6bc827593e
husni-robani <husnir2005@gmail.com>
2024-09-12 14:44:25 +0700

    define express-ws

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const controller = require("./app/controllers/appController");
+
+const express = require("express");
 const app = express();
+
+const expressWs = require("express-ws")(app);
 
 const corsOptions = {
   origin: ["http://localhost:8080"],
@@ -17,27 +21,13 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ========================================== database (comment by me)
-// const db = require("./app/models");
-// db.sequelize.sync();
-// ==========================================
-
-// never enable the code below in production
-// force: true will drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and Resync Database with { force: true }");
-//   // initial();
-// });
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Hello" });
-});
-
 // routes
 require("./app/routes/appRoutes")(app);
 
-// set port, listen for requests
+// websocket route
+const Controller = require("./app/controllers/appController");
+app.ws("/app/data", Controller.callmeWebSocket);
+
 const PORT = 7878;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
